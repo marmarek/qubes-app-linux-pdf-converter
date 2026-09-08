cb898549521a2cda5a3d1504d5418b45d11a47be
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2026-09-08 14:14:58 +0200

    tests: set timeout to cancelled qvm-convert-pdf
    'timeout' command sends a signal to qvm-convert-pdf, but it doesn't
    really force the process (or its children) to terminate. This is
    actually what the test is supposed to verify. Add a timeout to the wait,
    to not hang the whole test suite in case of a failure.

--- qubespdfconverter/tests/integ.py
+++ qubespdfconverter/tests/integ.py
@@ -429,7 +429,7 @@ with zipfile.ZipFile(filename, "w") as ods:
             'cp test.pdf orig.pdf; '
             'timeout --signal=INT 40 qvm-convert-pdf test.pdf 2>&1',
             passio_popen=True)
-        (stdout, _) = p.communicate()
+        (stdout, _) = p.communicate(timeout=60)
         self.assertNotEqual(p.returncode, 0,
             'Expected non-zero exit from interrupted conversion: {}'.format(stdout))
         self.assertNotEqual(
